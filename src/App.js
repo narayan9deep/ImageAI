469ef7269b59af0b7706b55e2e3b6db76aa55aa0
NARAYAN <narayan9deep@gmail.com>
2023-04-15 11:22:43 +0530

    Create reusable Navbar component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
-
+      <div className = "container mx-auto">
+        <Navbar/>
+      </div>
     </div>
   );
 }
---
+++ src/components/Navbar.js
@@ -0,0 +1,36 @@
+export default function Navbar() 
+{
+  return (
+    <nav>
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="flex items-center justify-between h-16">
+          <div className="flex-shrink-0">
+            <h1 className="h-8 w-8 font-semibold text-xl"> PhotoGPT </h1>
+          </div>
+          <div className="md:block">
+            <div className="ml-10 flex items-baseline space-x-4">
+              <a
+                href="#"
+                className="hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+              >
+                Home
+              </a>
+              <a
+                href="#"
+                className="hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+              >
+                Contact
+              </a>
+              <a
+                href="#"
+                className="hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+              >
+                About
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
